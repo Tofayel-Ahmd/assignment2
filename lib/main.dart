c1f7d946e2951307e9f7edb78ba531965a75ab0f
Tofaye <92934456+Tofaye@users.noreply.github.com>
2024-08-14 19:30:14 +0600

    Assignemnt2

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment-2',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Gretting App'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gretting App"),
+        backgroundColor: Colors.lime,
+      ),
+    );
+  }
+}
